a5b03b6fe5bed9a8815e477fe632a2fa65f44ca0
Bigted99 <ace4real999@gmail.com>
2025-03-06 07:59:56 -0800

    mobile view almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
-const menu = document.getElementById('#mobile-menu');
-const menuLinks = document.getElementById('#navbar-menu');
-// Display mobile menu
+// const menu = document.getElementById('#mobile-menu');
+// const menuLinks = document.getElementById('#navbar-menu');
+// // Display mobile menu
 
-function hideMenu (){
-   const mobileMenu = () =>{
-menu.classList.toggle('is-active');
-menuLinks.classList.toggle('active');
-}
-   menu.addEventListener('click', mobileMenu)
-}
+// function hideMenu (){
+//    const mobileMenu = () =>{
+// menu.classList.toggle('is-active');
+// menuLinks.classList.toggle('active');
+// }
+//    menu.addEventListener('click', mobileMenu)
+// }
 
 
-const videoTransition = document.getElementById('#myVideo');
- setTimeout(() =>{
-    videoTransition.style.display = "none";
- },5000);
+// const videoTransition = document.getElementById('#myVideo');
+//  setTimeout(() =>{
+//     videoTransition.style.display = "none";
+//  },5000);
 
- function greet () {
-   console.log('hello-world');
- }
+//  function greet () {
+//    console.log('hello-world');
+//  }
 
- console.log(greet())
+//  console.log(greet())
 
- hideMenu();
+//  hideMenu();
